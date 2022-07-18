933c7e9aca2add68d3c00742b8b2cf7091b2e9bb
Muhyideen Mujibat <96301638+MuhyideenMujibat@users.noreply.github.com>
2022-07-18 12:48:00 +0100

    Add files via upload

---
+++ sandbox.js
@@ -0,0 +1,7 @@
+var character = 'my name is muhyideen mujibat olaide';
+console.log(character);
+var inputs = document.querySelectorAll('input');
+console.log(inputs);
+inputs.forEach(function (input) {
+    console.log(input);
+});
---
+++ sandbox.ts
@@ -0,0 +1,11 @@
+const character = 'my name is muhyideen mujibat olaide';
+
+console.log(character);
+
+const inputs = document.querySelectorAll('input');
+
+console.log(inputs);
+
+inputs.forEach(input => {
+  console.log(input);
+});
